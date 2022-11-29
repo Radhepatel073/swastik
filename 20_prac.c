ca6b5992b24a8e202ee4450e9a8ebdce74214c86
RADHE PATEL <117585222+Radhepatel073@users.noreply.github.com>
2022-11-29 14:49:54 +0530

    Add files via upload
    swastik pattern in c programming

---
+++ 20_prac.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int j,i,no;
+printf("Enter the number:\n");
+ scanf("%d",&no);
+ /* Run loops*/
+ for(i=1;i<=no*2-1;i++)
+ {
+  for(j=1;j<=no*2-1;j++)
+  {
+/* Conditions for print stars*/
+ if(i==no||j==no||i==1&&j>no||i<no&&j==1||i>no&&j==no*2-1||i==no*2-1&&j<no)
+   {
+  printf(" *");
+   }
+
+
+   else
+   {
+  printf("  ");
+   }
+  }
+  printf("\n");
+ } 
+ 
+
+ return 0;
+}  
